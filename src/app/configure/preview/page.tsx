72c42f985beeda3f21f6845f790216d67e0bda9a
Venu005 <venusaiyalamanchili@gmail.com>
2024-06-20 16:05:17 +0530

    feat: summary page complete

---
+++ src/app/configure/preview/page.tsx
@@ -0,0 +1,29 @@
+//we get  id as search Param
+
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignPreview from "./DesignPreview";
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+  const configuration = await db.configuration.findUnique({
+    where: {
+      id,
+    },
+  });
+  if (!configuration) {
+    return notFound();
+  }
+  //pass some props
+  return <DesignPreview configuration={configuration} />;
+};
+
+export default Page;
